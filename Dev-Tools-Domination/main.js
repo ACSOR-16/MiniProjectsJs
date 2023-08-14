9dbc3858cb8f4f5661f118dd7a1614578f5ca2fd
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-08-14 11:44:24 -0500

    dev tools project

---
+++ Dev-Tools-Domination/main.js
@@ -0,0 +1,31 @@
+const dogs = [{name: "Snick", age: 2}, {name: "Hug", age: 8}];
+
+function makeGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing

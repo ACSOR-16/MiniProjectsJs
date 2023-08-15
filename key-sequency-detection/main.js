e16130c031c3a3b5ac132f12299232aa97c5cfc7
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-08-15 14:27:19 -0500

    mini project finish

--- key-sequency-detection/main.js
+++ key-sequency-detection/main.js
@@ -1,1 +1,18 @@
 console.log("Hello world");
+const pressed = [];
+const secretCode = "webos";
+
+window.addEventListener('keyup', (event) => {
+  console.log(event.key);
+  pressed.push(event.key);
+  pressed.splice(
+    -secretCode.length -1,
+    pressed.length - secretCode.length
+    );
+  
+  if (pressed.join("").includes(secretCode)) {
+    console.log("DING DING");
+    cornify_add();
+  }
+  console.log(pressed);
+});

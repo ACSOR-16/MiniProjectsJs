38257c5a8cd51a386d4e48c4dbe5f0732b7261c6
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-08-21 21:52:11 -0500

    styles of the file html

--- Webcam Fun/main.js
+++ Webcam Fun/main.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.querySelector("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector("snap");

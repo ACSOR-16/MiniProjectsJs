a3a83f8f8e0cf850ecc07ea200b654f9b1fc4406
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-08-25 14:48:19 -0500

    Event capture, propagation bubbling and once project finished

--- Event Capture, Propagation, Bubbling and Once/main.js
+++ Event Capture, Propagation, Bubbling and Once/main.js
@@ -1,1 +1,14 @@
 console.log("hello world");
+const divS = document.querySelectorAll("div");
+const button = document.querySelector("button");
+
+divS.forEach( div => div.addEventListener("click", logText, {capture: false, once: true}));
+button.addEventListener("click", () => {
+  console.log("click");
+}, {
+  once: true
+});
+
+function logText(event) {
+  console.log(this.classList.value);
+}

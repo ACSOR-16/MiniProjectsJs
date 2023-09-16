1082d49f7e1e001b61597e31e2000d443440d705
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-09-15 19:51:11 -0500

    updated: legos modal

---
+++ Animations-lego/main.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".modal");
+const legoImages = document.querySelectorAll(".superImage");
+const btnClose = document.querySelector(".modal__content--close");
+// ".hidden"
+
+console.log(legoImages);
+legoImages.forEach( lego => {
+  lego.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+  });
+});
+
+btnClose.addEventListener("click", () => {
+  console.log("here");
+  modal.classList.add("hidden");
+  modal.classList.remove("visible")
+});
+
